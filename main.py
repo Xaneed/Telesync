20bcd751b558c8da374cc8be3164e8e1ebd7d877
Xaneed <sa@siluetstudio.com>
2022-04-30 22:04:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from pyrogram import Client
+from configparser import ConfigParser
+
+# Load config
+config = ConfigParser()
+config.read('config.ini')
+
+# Setting up
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+chat_id = int(config['Telegram']['chat_id'])
+
+# Create application instance
+app = Client('userbot', api_id, api_hash)
+
+async def main():
+  async with app:
+    async for message in app.get_chat_history(chat_id):
+      if message.audio:
+        ext = os.path.splitext(message.audio.file_name)
+        filename = message.audio.performer + ' - ' + message.audio.title + ext[1]
+        await app.download_media(message, filename, progress=progress, progress_args=[filename])
+        print('', end='\n')
+
+async def progress(current, total, filename):
+    print('', end='\r')
+    print(filename, end='  ')
+    print(f"{current * 100 / total:.1f}%", end='\r')
+
+# Run application
+app.run(main())
